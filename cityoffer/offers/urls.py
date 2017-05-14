e6a8f8357120bcbd86b3c3deb0c2b4647894c772
marwaalalawy <marwa.alalawy@gmail.com>
2017-05-15 01:15:13 +0300

    login page-not completed

--- cityoffer/offers/urls.py
+++ cityoffer/offers/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index , name='index'),
+    url(r'^login/$',views.login, {'template_name': 'login.html'}
+)
 ]
--- cityoffer/offers/views.py
+++ cityoffer/offers/views.py
@@ -4,6 +4,17 @@ from django.http import HttpResponse
 from .models import Offer
 
 def index(request):
-	return HttpResponse("hello") 
+	return HttpResponse("hello")
+
+
+	def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+      if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect(reverse('home'))
+    else:
+        return HttpResponseRedirect('/accounts/invalid') 
 
 # Create your views here.
