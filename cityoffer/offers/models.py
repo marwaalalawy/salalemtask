0113a0b6fec5bb584d240deefa64980d94b87407
noor atyat <nooratyat92@gmail.com>
2017-05-14 23:56:06 +0300

    creat model and admin

--- cityoffer/offers/models.py
+++ cityoffer/offers/models.py
@@ -1,18 +1,18 @@
+# from django.db import models
+
+
+from __future__ import unicode_literals
+
 from django.db import models
+import datetime
+# Create your models here.
 
-class offer(models.Model):
-	companyName=models.CharField(max_length=200)
-	description=models.CharField(max_length=200)
-	date=models.CharField(max_length=200)
-	create_date=models.DateTimeField('date published')
-	location=models.CharField(max_length=200)
+class Offer(models.Model):
+    companyName=models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    date=models.CharField(max_length=200)
+    
+    location=models.CharField(max_length=200)
 
-
-#     def __str__(self):
-#     	return self.company_name +'_' +self.discription
-    	
-# class company(models.Model):
-# 	offer=models.Foreignkey(Offer,on_delete=models.CASCADE)
-# 	name=models.CharField(max_length=200)
-# 	email=models.CharField(max_length=200)
-# 	password=models.CharField(max_length=200)
+    def __str__(self):
+     	  return self.descriptions
--- cityoffer/offers/views.py
+++ cityoffer/offers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Offer
 
 def index(request):
 	return HttpResponse("hello") 
