369c50d71941633f4c2f4b4724da8f1debdcb3dc
marwaalalawy <marwa.alalawy@gmail.com>
2017-05-15 12:12:01 +0300

    login page by noor

--- cityoffer/offers/urls.py
+++ cityoffer/offers/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views 
 
+
 urlpatterns = [
     url(r'^$', views.index , name='index'),
-    url(r'^login/$',views.login, {'template_name': 'login.html'}
-)
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^login/$', views.login, {'template_name': 'login.html'}, name='login')
 ]
--- cityoffer/offers/views.py
+++ cityoffer/offers/views.py
@@ -8,13 +8,13 @@ def index(request):
 
 
 	def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-      if user is not None:
-        auth.login(request, user)
-        return HttpResponseRedirect(reverse('home'))
-    else:
-        return HttpResponseRedirect('/accounts/invalid') 
+    # username = request.POST['username']
+    # password = request.POST['password']
+    # user = authenticate(request, username=username, password=password)
+    #   if user is not None:
+    #     auth.login(request, user)
+         return HttpResponse("hi")
+    # else:
+    #      return HttpResponseRedirect('/accounts/invalid') 
 
 # Create your views here.
